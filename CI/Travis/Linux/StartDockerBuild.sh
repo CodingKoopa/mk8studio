4eea824ac6733b9ab0ae0a146ac1e5560a438db0
TheKoopaKingdom <thekoopakingdom@gmail.com>
2018-03-24 19:18:24 -0400

    Fixed Travis script environmental variable.

--- CI/Travis/Linux/StartDockerBuild.sh
+++ CI/Travis/Linux/StartDockerBuild.sh
@@ -3,7 +3,7 @@
 # Starts a Docker container and runs the build script.
 
 # The Docker container needs to be able to access the project root, to run the build script.
-PROJECT_ROOT_MOUNT_POINT="/mk8studio"
+export PROJECT_ROOT_MOUNT_POINT="/mk8studio"
 # In the Ubuntu 18.04 image installed by InstallDeps.sh, run the build script "Build.sh".
-docker run -e ${PROJECT_ROOT_MOUNT_POINT} -v $(pwd):${PROJECT_ROOT_MOUNT_POINT} ubuntu:18.04 \
+docker run -e PROJECT_ROOT_MOUNT_POINT -v $(pwd):${PROJECT_ROOT_MOUNT_POINT} ubuntu:18.04 \
   /bin/bash -ex "${PROJECT_ROOT_MOUNT_POINT}/CI/Travis/Linux/Build.sh"
