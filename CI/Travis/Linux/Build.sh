a2cf939b55e3bb7beb245b8fa57e5d1b34d7549c
TheKoopaKingdom <thekoopakingdom@gmail.com>
2018-03-24 19:07:09 -0400

    Documented dependencies, and added the qt5-default dependency so that qmake will work.

--- CI/Travis/Linux/Build.sh
+++ CI/Travis/Linux/Build.sh
@@ -9,8 +9,12 @@ cd ${PROJECT_ROOT_MOUNT_POINT}
 # Update the system.
 apt-get update
 # Install build dependencies. This is not done in "InstallDeps.sh" because they would be lost after
-# the "install" TravisCI step.
-apt-get install -y build-essential qtbase5-dev qttools5-dev
+# the "install" TravisCI step. The dependencies are:
+# - build-essential   GCC and other compiling utilities.
+# - qtbase5-dev       Base Qt libraries.
+# - qttools5-dev      Qt utilities such as uic for compiling UI files.
+# - qt5-default       Sets Qt5 as the default Qt version. Needed for qmake to run.
+apt-get install -y build-essential qtbase5-dev qttools5-dev qt5-default
 
 # Make and enter a build directory.
 mkdir Build && cd Build
