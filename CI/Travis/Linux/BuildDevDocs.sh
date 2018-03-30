79cf5467879d6b1efb7e86760b03d5640a78029e
Koopa <thekoopakingdom@gmail.com>
2018-03-30 01:35:24 -0400

    CI: Migrated Doxygen developer documentation building to Docker (#12)
    * Migrated Doxygen Travis build from the 14.04 package to the 18.04 package, because the latest version should be preferred. Also renamed the Docker dependency script acoordingly.
    
    * Fixed the dev doc Docker container not being able to access the build script.
    
    * Added graphviz, for dot.

--- CI/Travis/Linux/BuildDevDocs.sh
+++ CI/Travis/Linux/BuildDevDocs.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -ex
 
-# Builds developer documentation with Doxygen.
+# Builds developer documentation with Doxygen. This script is meant to be ran from an Ubuntu 18.04
+# Docker container, started from "StartDockerDevDocBuild.sh"
 
 # Enter the documentation directory.
-cd Docs
+cd ${PROJECT_ROOT_MOUNT_POINT}/Docs
+
+# Update the system.
+apt-get update
+# Install documentation build dependencies. This is not done in "InstallDeps.sh" because they would
+# be lost after the "install" TravisCI step. The dependencies are:
+# - doxygen   The main Doxygen executable.
+# - graphviz  The dot tool used for generating diagrams.
+apt-get install -y doxygen graphviz
+
 # Build the docmentation, and quit if any errors are encountered.
 doxygen
